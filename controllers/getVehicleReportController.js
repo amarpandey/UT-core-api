dff499e53480deeb4a7c9fc7a5debe292eeecc52
amar <amar.pandey@mindtree.com>
2024-05-02 12:41:49 +0530

    new routs to get report data

---
+++ controllers/getVehicleReportController.js
@@ -0,0 +1,33 @@
+console.log('inside controller');
+const getVehicleService = require('../services/getVehicleService');
+
+const getVehicleDetails = async (req, res)=>{
+    try{
+        console.log('inside getVehicleDetails controller:: ');
+        // Calling getVehicle service 
+        const userToken = req.query.token;
+        const validToken = userToken == 'dcc921361dbab401f92cbfb7c6a8752d49171B5760B97B650D5697E3C526857D0CBBF907' ? true : false;
+        if(validToken){
+            const reportFrom = (new Date(req.query.from).getTime() / 1000);
+            const reportTo = (new Date(req.query.to).setHours(23, 59) / 1000);
+            // const reportType = req.query.report;
+            console.log('token :: '+ userToken);
+            console.log('from :: '+ reportFrom);
+            console.log('to :: '+ reportTo);
+            console.log('reportType :: '+ reportType);
+            const report = true;
+            const vehicleData = await getVehicleService.getVehicleServices(userToken, reportFrom, reportTo, report);
+            res.send(vehicleData);
+        }else{
+            res.send('Invalid Token');
+        }
+        
+    }catch(err){
+        return err;
+    }
+    
+}
+
+module.exports = getVehicleDetails;
+
+
